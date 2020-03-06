a80b41982f1fb9960d157a9261fcb293ab877724
sarvanideekshitula <sarvanimini@gmail.com>
2020-03-06 09:32:49 +0530

    chore: Update migrations

--- migrations/20200305171327-create-cart.js
+++ migrations/20200305171327-create-cart.js
@@ -1,38 +1,36 @@
 'use strict';
 module.exports = {
 	up: (queryInterface, Sequelize) => {
-		return queryInterface.createTable('products', {
+		return queryInterface.createTable('carts', {
 			id: {
 				allowNull: false,
+				autoIncrement: true,
 				primaryKey: true,
 				type: Sequelize.INTEGER
 			},
-			name: {
-				type: Sequelize.STRING
-			},
-			price: {
+			productId: {
 				type: Sequelize.INTEGER
 			},
 			quantity: {
 				type: Sequelize.INTEGER
 			},
-			imageLink: {
-				type: Sequelize.STRING
+			price: {
+				type: Sequelize.INTEGER
 			},
-			category: {
-				type: Sequelize.STRING
+			totalPrice: {
+				type: Sequelize.INTEGER
 			},
 			createdAt: {
 				allowNull: false,
-				type: Sequelize.DATE,
+				type: Sequelize.DATE
 			},
 			updatedAt: {
 				allowNull: false,
-				type: Sequelize.DATE,
-			},
+				type: Sequelize.DATE
+			}
 		});
 	},
 	down: (queryInterface, Sequelize) => {
-		return queryInterface.dropTable('products');
+		return queryInterface.dropTable('carts');
 	}
 };
