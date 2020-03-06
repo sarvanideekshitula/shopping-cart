dfc4d8f18a2e32bc55418efea97aa319a72bdb9d
sarvanideekshitula <sarvanimini@gmail.com>
2020-03-06 09:33:44 +0530

    feat: DbOperations for handlers

--- src/utils/dbOperations.js
+++ src/utils/dbOperations.js
@@ -5,8 +5,62 @@ const retriveAllProducts = async () => {
 	return products;
 };
 
-const addProductsToDb = async (data) => {
-	await db.products.bulkCreate(data, {fields:['id', 'name', 'price', 'quantity', 'imageLink', 'category']});
+const retrieveProductById = async (productId) => {
+	const product = await db.products.findOne({ where: { id: productId } });	
+	return JSON.parse(JSON.stringify(product));
 };
 
-module.exports={retriveAllProducts, addProductsToDb};
+const retrieveProducts = async (productIds) => {
+	const products = await db.products.findAll({ where: { id: productIds } });
+	return JSON.parse(JSON.stringify(products));
+};
+
+const retriveByFilter = async (filter) => {
+	const products = await db.products.findAll({ where: { category: filter } });
+	return products;
+};
+
+const retriveAllCategories = async () => {
+	const categories = await db.products.aggregate('category', 'DISTINCT', { plain: false });
+	return categories.map(x=>x.DISTINCT);
+};
+
+const addProductsToDb = async (data) => {
+	await db.products.bulkCreate(data, { fields: ['id', 'name', 'price', 'quantity', 'imageLink', 'category'] });
+};
+
+const updateInventory = async (data) => {
+	await db.products.update({ quantity: data.quantity }, {where: {id: data.productId}  });
+};
+
+const addToCart = async (data) => {
+	const cartItem = await db.cart.create(data);
+	return cartItem;
+};
+
+const retrieveCartItem = async (productId) => {
+	const cartItem = await db.cart.findOne({ where: { productId: productId } });
+	return JSON.parse(JSON.stringify(cartItem));
+};
+
+const retrieveCart = async () => {
+	const cart = await db.cart.findAll();
+	return JSON.parse(JSON.stringify(cart));
+};
+
+const updateCart = async (id, data) => {
+	const cartItem = await db.cart.update({ quantity: data.quantity, totalPrice: data.totalPrice }, {where: {id: id}  });
+	return cartItem;
+};
+
+const deleteFromCart = async (cartItemId) => {
+	const cartItem = await db.cart.destroy({where: { id: cartItemId }});
+	return cartItem;
+};
+
+const clearCart = async () => {
+	const cartItem = await db.cart.destroy({truncate: true});
+	return cartItem;
+};
+
+module.exports = { retriveAllProducts, retrieveProducts, retrieveProductById, retriveByFilter, retriveAllCategories, addProductsToDb, updateInventory, retrieveCart, retrieveCartItem, addToCart, updateCart, deleteFromCart, clearCart };
--- tests/utilsTests/dbOperations.test.js
+++ tests/utilsTests/dbOperations.test.js
@@ -80,3 +80,208 @@ describe('The addProductsToDb function', () => {
 		done();
 	});
 });
+
+describe('The retrieveProductById function', () => {
+	const data = 
+		{
+			id: 1,
+			name : 'Apple - 1kg',
+			price: 210,
+			quantity: 10,
+			imageLink: 'https://techunic-eval4.s3.amazonaws.com/apple.jpg',
+			category: 'fruits',
+		};
+	it('should return product with given id when db read operation is successful', async(done) => {
+		const mockAddProducts = jest.spyOn(db.products, 'findOne');
+		mockAddProducts.mockResolvedValue(data);
+		await dbOperations.retrieveProductById(1);
+		expect(mockAddProducts).toHaveBeenCalled();
+		mockAddProducts.mockRestore();
+		done();
+	});
+	it('should throw an error if the db fetch operation fails', async (done) => {
+		const mockAddProducts = jest.spyOn(db.products, 'findOne');
+		mockAddProducts.mockResolvedValue();
+		mockAddProducts.mockRejectedValue(new Error('Error occurred'));
+		try {
+			await dbOperations.retrieveProductById(data);
+		} catch (error) {
+			expect(error.message).toBe('Error occurred');
+		}
+		mockAddProducts.mockRestore();
+		done();
+	});
+});
+
+describe('The retrieveProducts function', () => {
+	const data = [
+		{
+			id: 1,
+			name : 'Apple - 1kg',
+			price: 210,
+			quantity: 10,
+			imageLink: 'https://techunic-eval4.s3.amazonaws.com/apple.jpg',
+			category: 'fruits',
+		},
+		{
+			id: 2,
+			name : 'Apple - 1kg',
+			price: 210,
+			quantity: 10,
+			imageLink: 'https://techunic-eval4.s3.amazonaws.com/apple.jpg',
+			category: 'fruits',
+		}
+	];
+		
+	it('should return product with given ids when db read operation is successful', async(done) => {
+		const mockAddProducts = jest.spyOn(db.products, 'findAll');
+		mockAddProducts.mockResolvedValue(data);
+		await dbOperations.retrieveProducts([1,2]);
+		expect(mockAddProducts).toHaveBeenCalled();
+		mockAddProducts.mockRestore();
+		done();
+	});
+	it('should throw an error if the db fetch operation fails', async (done) => {
+		const mockAddProducts = jest.spyOn(db.products, 'findAll');
+		mockAddProducts.mockResolvedValue();
+		mockAddProducts.mockRejectedValue(new Error('Error occurred'));
+		try {
+			await dbOperations.retrieveProducts([1,2]);
+		} catch (error) {
+			expect(error.message).toBe('Error occurred');
+		}
+		mockAddProducts.mockRestore();
+		done();
+	});
+});
+
+describe('The retriveByFilter function', () => {
+	const data = [
+		{
+			id: 1,
+			name : 'Apple - 1kg',
+			price: 210,
+			quantity: 10,
+			imageLink: 'https://techunic-eval4.s3.amazonaws.com/apple.jpg',
+			category: 'fruits',
+		},
+		{
+			id: 2,
+			name : 'Apple - 1kg',
+			price: 210,
+			quantity: 10,
+			imageLink: 'https://techunic-eval4.s3.amazonaws.com/apple.jpg',
+			category: 'fruits',
+		}
+	];
+		
+	it('should return product with given filter when db read operation is successful', async(done) => {
+		const mockAddProducts = jest.spyOn(db.products, 'findAll');
+		mockAddProducts.mockResolvedValue(data);
+		await dbOperations.retriveByFilter('fruits');
+		expect(mockAddProducts).toHaveBeenCalled();
+		mockAddProducts.mockRestore();
+		done();
+	});
+	it('should throw an error if the db fetch operation fails', async (done) => {
+		const mockAddProducts = jest.spyOn(db.products, 'findAll');
+		mockAddProducts.mockResolvedValue();
+		mockAddProducts.mockRejectedValue(new Error('Error occurred'));
+		try {
+			await dbOperations.retriveByFilter('fruits');
+		} catch (error) {
+			expect(error.message).toBe('Error occurred');
+		}
+		mockAddProducts.mockRestore();
+		done();
+	});
+});
+
+describe('The retriveAllCategories function', () => {
+	const data = [
+		'fruits', 'household'
+	];
+		
+	it('should return product categories with when db read operation is successful', async(done) => {
+		const mockAddProducts = jest.spyOn(db.products, 'aggregate');
+		mockAddProducts.mockResolvedValue(data);
+		await dbOperations.retriveAllCategories();
+		expect(mockAddProducts).toHaveBeenCalled();
+		mockAddProducts.mockRestore();
+		done();
+	});
+	it('should throw an error if the db fetch operation fails', async (done) => {
+		const mockAddProducts = jest.spyOn(db.products, 'aggregate');
+		mockAddProducts.mockResolvedValue();
+		mockAddProducts.mockRejectedValue(new Error('Error occurred'));
+		try {
+			await dbOperations.retriveAllCategories();
+		} catch (error) {
+			expect(error.message).toBe('Error occurred');
+		}
+		mockAddProducts.mockRestore();
+		done();
+	});
+});
+
+describe('The updateInventory function', () => {
+	const data = {
+		productId: 1,
+		name : 'Apple - 1kg',
+		price: 210,
+		quantity: 10,
+		imageLink: 'https://techunic-eval4.s3.amazonaws.com/apple.jpg',
+		category: 'fruits',
+	};
+		
+	it('should return updated product when db write operation is successful', async(done) => {
+		const mockAddProducts = jest.spyOn(db.products, 'update');
+		mockAddProducts.mockResolvedValue(data);
+		await dbOperations.updateInventory(data);
+		expect(mockAddProducts).toHaveBeenCalled();
+		mockAddProducts.mockRestore();
+		done();
+	});
+	it('should throw an error if the db fetch operation fails', async (done) => {
+		const mockAddProducts = jest.spyOn(db.products, 'update');
+		mockAddProducts.mockResolvedValue();
+		mockAddProducts.mockRejectedValue(new Error('Error occurred'));
+		try {
+			await dbOperations.updateInventory(data);
+		} catch (error) {
+			expect(error.message).toBe('Error occurred');
+		}
+		mockAddProducts.mockRestore();
+		done();
+	});
+});
+
+describe('The addToCart function', () => {
+	const data = {
+		productId: 1,
+		price: 210,
+		quantity: 1,
+		totalPrice: 210
+	};
+		
+	it('should return updated product when db write operation is successful', async(done) => {
+		const mockAddProducts = jest.spyOn(db.cart, 'create');
+		mockAddProducts.mockResolvedValue(data);
+		await dbOperations.addToCart(data);
+		expect(mockAddProducts).toHaveBeenCalled();
+		mockAddProducts.mockRestore();
+		done();
+	});
+	it('should throw an error if the db fetch operation fails', async (done) => {
+		const mockAddProducts = jest.spyOn(db.cart, 'create');
+		mockAddProducts.mockResolvedValue();
+		mockAddProducts.mockRejectedValue(new Error('Error occurred'));
+		try {
+			await dbOperations.addToCart(data);
+		} catch (error) {
+			expect(error.message).toBe('Error occurred');
+		}
+		mockAddProducts.mockRestore();
+		done();
+	});
+});
